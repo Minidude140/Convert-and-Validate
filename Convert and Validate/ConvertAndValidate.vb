c5a4828685d8447004634aeafd0e2e913a41f9fe
Zac <zac.minidude140@Gmail.com>
2023-09-23 12:08:03 -0600

    Added quit loop functionality

--- Convert and Validate/ConvertAndValidate.vb
+++ Convert and Validate/ConvertAndValidate.vb
@@ -11,7 +11,50 @@ Option Strict On
 Module ConvertAndValidate
 
     Sub Main()
+        'for testing the function add this to your Sub Main()
 
+        Dim aValidNumber As Integer
+        Dim userResponse As String
+
+        Do
+
+            Console.WriteLine($"Enter a number:" & vbLf & "(enter 'Q' to quit)")
+
+            userResponse = Console.ReadLine()
+
+            Select Case userResponse
+                Case = "Q", "q"
+                    Exit Do
+                Case Else
+                    If ConversionValid(userResponse, aValidNumber) = True Then
+
+                        Console.WriteLine($"{userResponse} converted successfully to {aValidNumber}!")
+
+                    Else
+
+                        Console.WriteLine($"Oops, {userResponse} does not seem to be a number")
+
+                    End If
+            End Select
+
+        Loop
     End Sub
+
+    'make this work
+    Function ConversionValid(userInput As String, convertedInteger As Integer) As Boolean
+
+        Dim status As Boolean
+
+        Try
+            convertedInteger = CInt(userInput)
+            status = True
+            'aValidNumber = convertedInteger
+        Catch ex As Exception
+            status = False
+        End Try
+
+        Return status
+    End Function
+
 
 End Module
